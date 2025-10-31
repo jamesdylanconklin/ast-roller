873e54ec682e2f460a2068dbbc40b509632198d3
James Conklin <jamesdylanconklin@gmail.com>
2025-10-31 14:25:22 -0700

    Error display in main

--- src/ast_roller/__main__.py
+++ src/ast_roller/__main__.py
@@ -7,14 +7,20 @@ def main():
     opts, args = opt_parser.parse_args()
 
     roll_string = " ".join(args) if args else '1d20'
-    parse_tree = parser.parse(roll_string)
-    eval_tree = transformer.transform(parse_tree)
-    result_tree = eval_tree.evaluate()
-  
-    if opts.verbose:
-        print(result_tree.pretty_print())
-    else:
-        print(result_tree.raw_result)
+
+    try:
+        parse_tree = parser.parse(roll_string)
+        eval_tree = transformer.transform(parse_tree)
+        result_tree = eval_tree.evaluate()
+
+        if opts.verbose:
+            print(result_tree.pretty_print())
+        else:
+            print(result_tree.raw_result)
+
+    except Exception as e:
+        print(f"Could not process roll string {roll_string}")
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
-  main()
+    main()

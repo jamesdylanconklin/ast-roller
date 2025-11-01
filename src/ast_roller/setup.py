13ba8a87b0848b13e71960f5232dd8b958e662f9
James Conklin <jamesdylanconklin@gmail.com>
2025-10-31 19:49:58 -0700

    Remove setup.py

--- src/ast_roller/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="ast_roller",
-    version="0.1.0",
-    py_modules=["grammar", "evaluators", "results"],
-    install_requires=["lark==1.1.9"],
-)

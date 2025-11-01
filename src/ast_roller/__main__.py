49c69987786aeac0442b1b27efb94726765bf715
James Conklin <jamesdylanconklin@gmail.com>
2025-10-31 19:50:20 -0700

    package with cli script

--- src/ast_roller/__main__.py
+++ src/ast_roller/__main__.py
@@ -1,26 +1,4 @@
-from ast_roller.grammar import parser, transformer
-import optparse
-
-def main():
-    opt_parser = optparse.OptionParser()
-    opt_parser.add_option('-v', dest='verbose', action='store_true')
-    opts, args = opt_parser.parse_args()
-
-    roll_string = " ".join(args) if args else '1d20'
-
-    try:
-        parse_tree = parser.parse(roll_string)
-        eval_tree = transformer.transform(parse_tree)
-        result_tree = eval_tree.evaluate()
-
-        if opts.verbose:
-            print(result_tree.pretty_print())
-        else:
-            print(result_tree.raw_result)
-
-    except Exception as e:
-        print(f"Could not process roll string {roll_string}")
-        print(f"Error: {e}")
+from ast_roller.main import main
 
 if __name__ == "__main__":
     main()
